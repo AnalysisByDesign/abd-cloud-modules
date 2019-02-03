12492a546f2ddb76429acb85b8d1ad9b0d4189cb
Dave Rix <dave@analysisbydesign.co.uk>
2019-02-03 23:38:58 +0000

    Final naming changes

--- compute/docker-repository/outputs.tf
+++ compute/docker-repository/outputs.tf
@@ -1,3 +1,7 @@
+output "repository_name" {
+  value = "${join("", aws_ecr_repository.this.*.name)}"
+}
+
 output "repository_arn" {
   value = "${join("", aws_ecr_repository.this.*.arn)}"
 }
@@ -6,10 +10,6 @@ output "repository_url" {
   value = "${join("", aws_ecr_repository.this.*.repository_url)}"
 }
 
-output "ecr_repository_id" {
-  value = "${join("", aws_ecr_repository.this.*.repository_id)}"
-}
-
-output "repository_name" {
-  value = "${join("", aws_ecr_repository.this.*.name)}"
+output "registry_id" {
+  value = "${join("", aws_ecr_repository.this.*.registry_id)}"
 }
