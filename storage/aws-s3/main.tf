b8dc387743b400dbaafaf6c29f15a628c92ba440
Dave Rix <dave@analysisbydesign.co.uk>
2019-03-21 22:37:29 +0000

    Minor fix

--- storage/aws-s3/main.tf
+++ storage/aws-s3/main.tf
@@ -43,7 +43,7 @@ resource "aws_s3_bucket" "bucket" {
 # If we have policy defined as text we apply this resource
 resource "aws_s3_bucket_policy" "bucket_policy" {
   count  = "${var.count > 0 && var.bucket_policy != "" ? 1 : 0}"
-  bucket = "${aws_s3_bucket.this.id}"
+  bucket = "${aws_s3_bucket.bucket.id}"
   policy = "${var.bucket_policy}"
 }
 
