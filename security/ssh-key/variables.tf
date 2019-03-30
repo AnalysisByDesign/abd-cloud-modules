548cb45f3709bebb66c9db628244731761659b99
Dave Rix <dave@analysisbydesign.co.uk>
2019-03-29 20:37:37 -0400

    Add SSH key module

---
+++ security/ssh-key/variables.tf
@@ -0,0 +1,16 @@
+# --------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# --------------------------------------------------------------------------------------------
+# Required -----------------------------------------------------------------------------------
+# Optional -----------------------------------------------------------------------------------
+
+variable "name" {
+  description = "Name of SSH key to upload"
+  type        = "string"
+}
+
+variable "public_key_file" {
+  description = "Physical file location of public key part to upload"
+  type        = "string"
+}
