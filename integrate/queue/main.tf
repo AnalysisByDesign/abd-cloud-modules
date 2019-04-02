705457d1c8af015e48ca94fca4695e30c578f58a
Dave Rix <dave@analysisbydesign.co.uk>
2019-04-02 22:23:34 +0100

    Add queue modile with SQS to start

---
+++ integrate/queue/main.tf
@@ -0,0 +1,34 @@
+# --------------------------------------------------------------------------------------------
+# SQS Queue
+# --------------------------------------------------------------------------------------------
+
+resource "aws_sqs_queue" "without_redrive" {
+  count = "${ var.count > 0 ? (var.deadletter_enable ? 0 : 1) : 0}"
+
+  name       = "${var.name}"
+  fifo_queue = "${var.fifo}"
+
+  message_retention_seconds = "${var.message_retention_seconds}"
+
+  max_message_size          = "${var.max_message_size}"
+  delay_seconds             = "${var.delay_seconds}"
+  receive_wait_time_seconds = "${var.receive_wait_time_seconds}"
+
+  tags = "${merge(var.common_tags, map("Name", format("%s", var.name)))}"
+}
+
+resource "aws_sqs_queue" "with_redrive" {
+  count = "${ var.count > 0 ? (var.deadletter_enable ? 1 : 0) : 0}"
+
+  name       = "${var.name}"
+  fifo_queue = "${var.fifo}"
+
+  message_retention_seconds = "${var.message_retention_seconds}"
+
+  max_message_size          = "${var.max_message_size}"
+  delay_seconds             = "${var.delay_seconds}"
+  receive_wait_time_seconds = "${var.receive_wait_time_seconds}"
+
+  redrive_policy = "{\"deadLetterTargetArn\":\"${var.deadletter_arn}\",\"maxReceiveCount\":4}"
+  tags           = "${merge(var.common_tags, map("Name", format("%s", var.name)))}"
+}
---
+++ integrate/queue/variables.tf
@@ -0,0 +1,57 @@
+# --------------------------------------------------------------------------------------------
+# SQS Queue
+# --------------------------------------------------------------------------------------------
+
+# Required -----------------------------------------------------------------------------------
+variable "name" {
+  description = "Name"
+  type        = "string"
+}
+
+variable "common_tags" {
+  description = "A map of tags to add to all resources"
+  type        = "map"
+}
+
+# Optional -----------------------------------------------------------------------------------
+
+variable "count" {
+  description = "How many queues to build"
+  default     = 0
+}
+
+variable "fifo" {
+  description = "Do we want a FIFO based queue"
+  default     = false
+}
+
+variable "message_retention_seconds" {
+  description = "Message retention period (seconds)"
+  default     = 345600
+}
+
+variable "max_message_size" {
+  description = "Max message size (bytes)"
+  default     = 262144
+}
+
+variable "delay_seconds" {
+  description = "Delivery delay in (seconds)"
+  default     = 0
+}
+
+variable "receive_wait_time_seconds" {
+  description = "Receive wait time (seconds)"
+  default     = 0
+}
+
+variable "deadletter_enable" {
+  description = "Do we have a deadletter queue?"
+  default     = false
+}
+
+variable "deadletter_arn" {
+  description = "Dead-letter queue arn"
+  type        = "string"
+  default     = ""
+}
