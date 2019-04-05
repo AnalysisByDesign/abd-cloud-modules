21704d3a163ff72c7ecf81adcd5698a13f2be61e
Dave Rix <dave@analysisbydesign.co.uk>
2019-04-05 10:37:06 +0100

    Add NLB module

---
+++ compute/nlb/main.tf
@@ -0,0 +1,47 @@
+# -----------------------------------------------------------------------------
+# Elastic Load Balancer
+# -----------------------------------------------------------------------------
+
+resource "aws_lb" "nlb" {
+  load_balancer_type = "network"
+  name               = "${var.name}"
+  internal           = true
+  subnets            = ["${var.subnet_ids}"]
+
+  enable_cross_zone_load_balancing = "${var.cross_zone_load_balancing}"
+
+  tags = "${merge(var.common_tags, var.lb_tags, map("Name", format("%s", var.name)))}"
+}
+
+resource "aws_lb_target_group" "nlb" {
+  name   = "${var.name}"
+  vpc_id = "${var.vpc_id}"
+
+  port     = "${var.ingress_port}"
+  protocol = "TCP"
+
+  target_type = "${var.target_type}"
+  slow_start  = "${var.slow_start}"
+
+  health_check {
+    interval = "${var.healthcheck_interval}"
+    port     = "${var.instance_port}"
+    protocol = "${var.protocol}"
+
+    healthy_threshold   = "${var.healthcheck_healthy_threshold}"
+    unhealthy_threshold = "${var.healthcheck_unhealthy_threshold}"
+  }
+
+  tags = "${merge(var.common_tags, map("Name", format("%s", var.name)))}"
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.nlb.arn}"
+  port              = "${var.ingress_port}"
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.nlb.arn}"
+    type             = "forward"
+  }
+}
---
+++ compute/nlb/variables.tf
@@ -0,0 +1,88 @@
+# --------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# --------------------------------------------------------------------------------------------
+
+variable "name" {
+  description = "The name of the load balancer"
+  type        = "string"
+}
+
+variable "vpc_id" {
+  description = "The VPC id for the target group"
+  type        = "string"
+}
+
+variable "subnet_ids" {
+  description = "The VPC subnet ids to load balance across"
+  type        = "list"
+}
+
+variable "common_tags" {
+  description = "A map of tags to add to all resources"
+  type        = "map"
+}
+
+# --------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# --------------------------------------------------------------------------------------------
+
+variable "cross_zone_load_balancing" {
+  description = "Enable cross zone load balancing"
+  default     = true
+}
+
+variable "lb_tags" {
+  description = "Additional tags for the NLB"
+  type        = "map"
+  default     = {}
+}
+
+variable "ingress_port" {
+  description = "The ingress port for the LB to listen on"
+  type        = "string"
+  default     = "22"
+}
+
+variable "instance_port" {
+  description = "The instance port for the LB to connect to"
+  type        = "string"
+  default     = "22"
+}
+
+variable "slow_start" {
+  description = "Slow start timeout before sending requests to the host"
+  type        = "string"
+  default     = "30"
+}
+
+variable "target_type" {
+  description = "LB target instance type"
+  type        = "string"
+  default     = "instance"
+}
+
+variable "healthcheck_interval" {
+  description = "Healthcheck interval"
+  type        = "string"
+  default     = "30"
+}
+
+variable "healthcheck_healthy_threshold" {
+  description = "Healthcheck healthy threshold"
+  type        = "string"
+  default     = "2"
+}
+
+variable "healthcheck_unhealthy_threshold" {
+  description = "Healthcheck unhealthy threshold"
+  type        = "string"
+  default     = "2"
+}
+
+variable "protocol" {
+  default     = "TCP"
+  type        = "string"
+  description = "Should be one of TCP, HTTP, HTTPS or TLS."
+}
