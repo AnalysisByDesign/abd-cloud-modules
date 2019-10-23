2580d966fec41132940851a139c4fd1c865a0a01
Dave Rix <dave@analysisbydesign.co.uk>
2019-10-23 21:28:20 +0100

    Add ESC cluster module

--- compute/ecs/cluster/main.tf
+++ compute/ecs/cluster/main.tf
@@ -1,10 +1,14 @@
-################################################################
-#
-# CORE: MAIN.TF Cluster.
-# Provides an ECS cluster.
-#
-################################################################
+# -----------------------------------------------------------------------------
+# ECS Cluster
+# -----------------------------------------------------------------------------
 
 resource "aws_ecs_cluster" "this" {
   name = "${var.name}"
+
+  # setting {
+  #   name  = "containerInsights"
+  #   value = "${var.ecs_insights}"
+  # }
+
+  tags = "${merge(var.common_tags, var.ecs_tags, map("Name", format("%s", var.name)))}"
 }
--- compute/ecs/cluster/variables.tf
+++ compute/ecs/cluster/variables.tf
@@ -3,6 +3,28 @@
 # --------------------------------------------------------------------------------------------
 
 variable "name" {
-  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  description = "The name of the cluster"
   type        = "string"
+}
+
+variable "common_tags" {
+  description = "A map of tags to add to all resources"
+  type        = "map"
+}
+
+# --------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# --------------------------------------------------------------------------------------------
+
+variable "ecs_insights" {
+  description = "Enable or disable Container Insights"
+  type        = "string"
+  default     = "disabled"
+}
+
+variable "ecs_tags" {
+  description = "Additional tags for the cluster"
+  type        = "map"
+  default     = {}
 }
