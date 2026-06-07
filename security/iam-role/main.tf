5ad1c7b57d57436c3f15424babe2b7b8c3a9c735
Dave Rix <dave@analysisbydesign.co.uk>
2026-06-07 12:57:34 +0100

    Add session duration to iam_role

--- security/iam-role/main.tf
+++ security/iam-role/main.tf
@@ -6,11 +6,12 @@
 # Multiple role creation
 # --------------------------------------------------------------------------------------------
 resource "aws_iam_role" "iam-role" {
-  count              = var.required ? length(var.roles) : 0
-  name               = lookup(var.roles[count.index], "name")
-  path               = var.path
-  description        = "Terraform - ${lookup(var.roles[count.index], "description")}"
-  assume_role_policy = lookup(var.roles[count.index], "assume_role_policy")
+  count                = var.required ? length(var.roles) : 0
+  name                 = lookup(var.roles[count.index], "name")
+  max_session_duration = lookup(var.roles[count.index], "max_session_duration", 3600)
+  path                 = var.path
+  description          = "Terraform - ${lookup(var.roles[count.index], "description")}"
+  assume_role_policy   = lookup(var.roles[count.index], "assume_role_policy")
 }
 
 resource "aws_iam_role_policy_attachment" "iam-role" {
@@ -21,4 +22,3 @@ resource "aws_iam_role_policy_attachment" "iam-role" {
 }
 
 # --------------------------------------------------------------------------------------------
-
